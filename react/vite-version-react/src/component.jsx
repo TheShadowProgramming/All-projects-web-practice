0d75d26fda29a09dc8679b209973381ecac6fa04
Monish Mishra <monishmprogram@gmail.com>
2024-04-19 12:17:38 +0530

    react basics

---
+++ react/vite-version-react/src/component.jsx
@@ -0,0 +1,16 @@
+const divStyle = {
+    color: "red",
+    border: "2px solid red"
+}
+
+function Component() {
+    return (
+        <>
+        <div style={divStyle}>
+
+        </div>
+        </>
+    )
+}
+
+export default Component;
