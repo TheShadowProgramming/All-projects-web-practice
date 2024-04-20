e4b6d4c7302291f41df1074a08a57eb11a67ef91
TheShadowProgramming <monishmprogram@gmail.com>
2024-04-21 00:24:39 +0530

    forof loop and forin loop

--- express-zod-jwt/App/testFile.js
+++ express-zod-jwt/App/testFile.js
@@ -4,44 +4,70 @@ I test small blocks of code here from the big project, pretty basic way to debug
 
 */
 
-const fs = require("fs");
-const z = require("zod");
+//onst fs = require("fs");
+//onst z = require("zod";
+//
+//unction importDataFunction() {
+// try {
+//   return JSON.parse(fs.readFileSync("../files/todoServer.JSON", "utf-8"));
+// } catch (err) {
+//   console.log(err);
+// }
+//
+//
+//unction exportDataFunction(importData) {
+// try {
+//   fs.writeFileSync(
+//     "../files/todoServer.JSON",
+//     JSON.stringify(importData, null, 2)
+//   );
+// } catch (err) {
+//   console.log(err);
+// }
+//
+//
+//et putParsedBody = {
+// data: {
+//   title: "Buy Gatorade",
+//   completed: true,
+// },
+//;
+//et IdNo = "aYPG";
+//et importData = importDataFunction();
+//or (let i = 0; i < importData.data.length; i++) {
+// if (importData.data[i].Id == IdNo) {
+//   let oldId = importData.data[i].Id;
+//   importData.data[i] = putParsedBody.data;
+//   importData.data[i].Id = oldId;
+//   exportDataFunction(importData);
+//   console.log("success");
+// } else {
+//   console.log("failure");
+// }
+//
+//
+const map = new Map();
 
-function importDataFunction() {
-  try {
-    return JSON.parse(fs.readFileSync("../files/todoServer.JSON", "utf-8"));
-  } catch (err) {
-    console.log(err);
-  }
+map.set("name", "Monish")
+map.set("age", "21")
+map.set("sex", "male")
+
+const myObj = {
+  "name":"Monish",
+  "age":21,
+  "sex":"Male"
 }
 
-function exportDataFunction(importData) {
-  try {
-    fs.writeFileSync(
-      "../files/todoServer.JSON",
-      JSON.stringify(importData, null, 2)
-    );
-  } catch (err) {
-    console.log(err);
-  }
+// iterating over objects and maps 
+
+for (const [key, value] of map) {
+  console.log(`the key of the map object is ${key} and the value of the map object is ${value}`)
 }
 
-let putParsedBody = {
-  data: {
-    title: "Buy Gatorade",
-    completed: true,
-  },
-};
-let IdNo = "aYPG";
-let importData = importDataFunction();
-for (let i = 0; i < importData.data.length; i++) {
-  if (importData.data[i].Id == IdNo) {
-    let oldId = importData.data[i].Id;
-    importData.data[i] = putParsedBody.data;
-    importData.data[i].Id = oldId;
-    exportDataFunction(importData);
-    console.log("success");
-  } else {
-    console.log("failure");
-  }
+for (const key of myObj) {
+  console.log(key) // this would throw an error
+}
+
+for (const key in myObj) {
+  console.log(key) // this won't throw an error but it can misbehave since this loop inherits properties from its proto-type
 }
