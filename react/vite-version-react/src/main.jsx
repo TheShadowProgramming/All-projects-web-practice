da86d6fce8d4149b86431d721d4d5c9589c51927
Monish Mishra <monishmprogram@gmail.com>
2024-05-01 17:42:31 +0530

    evaluated expressions in jsx

--- react/vite-version-react/src/main.jsx
+++ react/vite-version-react/src/main.jsx
@@ -8,7 +8,10 @@ const randomVariable = "this is an experimental variable"
 
 const firstReactElement = (
   <p>Im using the randomVariable here through this {randomVariable}</p>
-)
+) // another thing to note here is that randomVariable used here is
+// an instance of evaluated expression, i.e. we're not writing pure js here 
+// in this curly brace's scope, we're writing the final outcome of the js 
+// working under the hood of the react
 
 const anotherReactElement = (
   "div", // expects the type of html tag
