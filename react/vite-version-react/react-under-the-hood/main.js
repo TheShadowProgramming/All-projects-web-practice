d86840ee27a68a953c1d04f6bfdf03cd68870321
Monish Mishra <monishmprogram@gmail.com>
2024-05-01 14:01:20 +0530

    custom React file and understood react under the \ hood

--- react/vite-version-react/react-under-the-hood/main.js
+++ react/vite-version-react/react-under-the-hood/main.js
@@ -1,1 +1,20 @@
 // let's say we wanna create an element and push it inside the experimental div that we created
+const reactElement = {
+  type: "div",
+  props: {
+    style: "background-color: aqua;",
+    class: "button",
+  },
+  children: "<p>something inside the div</p>",
+};
+
+const mainContainer = document.querySelector("#root");
+
+function reactRender(reactElement, mainContainer) {
+  const newElement = document.createElement(reactElement.type);
+  newElement.innerHTML = reactElement.children;
+  for (const prop in reactElement.props) {
+    newElement.setAttribute(prop, reactElement.props[prop]);
+  }
+  mainContainer.appendChild(newElement);
+}
