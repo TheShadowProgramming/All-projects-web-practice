71050d1ceed84a0b9a5ea6a93522f92538379ec0
Monish Mishra <monishmprogram@gmail.com>
2024-05-06 12:59:23 +0530

    kk

--- react/counter-react/src/App.jsx
+++ react/counter-react/src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  let count = 5;
 
   return (
     <>
       <div>
-        counter react
+        count is : {count}
       </div>
+
+      <button>Add Value</button>
+      <br />
+      <button>decrease value</button>
+      <br />
+      <button>remove value</button>
     </>
   )
 }
@@ -21,3 +25,4 @@ export default App
 // app.jsx se we import the functions in the main.jsx and uske andar hi we use the .render syntax
 // jsx functions are called when we use the / operator after the name of the function inside the 
 // in order to return html using the jsx syntax, <> and </> is used inside the return function
+// for using variables inside a jsx syntax, we use curly brace syntax
