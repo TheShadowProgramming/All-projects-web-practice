2425484ec92cfdb2cacc8ea101fb0938995be2b9
Monish Mishra <monishmprogram@gmail.com>
2024-05-06 13:02:02 +0530

    ..

--- react/counter-react/src/App.jsx
+++ react/counter-react/src/App.jsx
@@ -25,3 +25,5 @@ export default App
 // app.jsx se we import the functions in the main.jsx and uske andar hi we use the .render syntax
 // jsx functions are called when we use the / operator after the name of the function inside the 
 // in order to return html using the jsx syntax, <> and </> is used inside the return function
+// babel works under the hood and converts all the modern js syntax into vanilla js 
+// in order to support jsx syntax in old browsers and all types of browsers
