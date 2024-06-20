15a82587ae945c2e04db9ef53cc28f0c7ae5e3d1
Monish Mishra <monishmprogram@gmail.com>
2024-06-21 00:55:54 +0530

    ..

--- react/reuse-component-react/component-react/src/App.jsx
+++ react/reuse-component-react/component-react/src/App.jsx
@@ -33,5 +33,11 @@ function App() {
 // it's better to make a components folder in the src folder where we'll write code for every component, and seperate styling for every component
 // react bootstrap if the framework to bootstrap your react project, react bootstrap se components utha rahe to 
 // className keyword 
+// ye bhadwa windows me case insesitivity hai bc, matlab windows considers capital letter file same as small letter file
+// use react bootstrap only for styling react components since react bootstrap under the hood uses virtual DOM and the actual DOM
+// react snippets extension for rfce and other shortcut keys, control + space for some component that we haven't imported yet
+// that will automatically write the import statement for that component
+// react bootstrap se components import karo, fir individual component files ke css files se style karo
+
 
 export default App
